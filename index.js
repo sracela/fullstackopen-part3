a6c70cfcc3a489617ea3de7ea0912aef8e863780
sracela <saracelalfonso@gmail.com>
2020-10-21 00:51:51 +0200

    3.3: Phonebook backend step3

--- index.js
+++ index.js
@@ -41,6 +41,20 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => {
+        // console.log(note.id, typeof note.id, id, typeof id, note.id === id)
+        return person.id === id
+    }) 
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
