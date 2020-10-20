26d3b39ad6a2bdb7b28c6d71f573f02019b79594
sracela <saracelalfonso@gmail.com>
2020-10-21 01:07:32 +0200

    3.5: Phonebook backend step5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     {
       "name": "Arto Hellas",
@@ -59,6 +61,31 @@ app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     persons = persons.filter(person => person.id !== id)
     response.status(204).end()
+})
+
+function getRandomId(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+  
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    // if (!body.content) {
+    //   return response.status(400).json({ 
+    //     error: 'content missing' 
+    //   })
+    // }
+    console.log(body)
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: getRandomId(1000),
+    }
+
+    persons = persons.concat(person)
+
+response.json(person)
 })
 
 const PORT = 3001
