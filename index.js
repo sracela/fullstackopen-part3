0e7563e7401d06378c9db4cd2aeaf6eb4aa6f95b
sracela <saracelalfonso@gmail.com>
2020-10-26 00:27:45 +0100

    3.15: Phonebook database, step3

--- index.js
+++ index.js
@@ -83,10 +83,19 @@ app.get('/api/persons/:id', (request, response) => {
   })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-    response.status(204).end()
+// app.delete('/api/persons/:id', (request, response) => {
+//     const id = Number(request.params.id)
+//     persons = persons.filter(person => person.id !== id)
+//     response.status(204).end()
+// })
+
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Contact.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    // .catch(error => next(error))
 })
 
 
